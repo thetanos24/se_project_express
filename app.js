cedc7ba633481836141aad2f3379f46f8e231825
Karen Tano <thetanos24@gmail.com>
2026-02-09 20:56:49 -0700

    updated model schemas

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
 const { PORT = 3001 } = process.env;
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/wtwr_db")
+  .then(() => {
+    console.log("Connected to DB");
+  })
+  .catch(console.error);
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
