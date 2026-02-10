ccbea3660fec194f504dee1928b00da29e862581
Karen Tano <thetanos24@gmail.com>
2026-02-09 20:58:47 -0700

    error fixed on schema

--- models/clothingitem.js
+++ models/clothingitem.js
@@ -17,24 +17,24 @@ const clothingitemSchema = new mongoose.Schema({
     type: String,
     required: true,
     validate: {
-          validator(value) {
-            return validator.isURL(value);
-          },
-          message: "You must enter a valid URL",
-        },
-  }
+      validator(value) {
+        return validator.isURL(value);
+      },
+      message: "You must enter a valid URL",
+    },
+  },
   owner: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "user",
     required: true,
   },
   likes: [
-  {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "user",
-    default: [],
-  },
-],
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+      default: [],
+    },
+  ],
   createdAt: {
     type: Date,
     default: Date.now,
