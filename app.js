aac693cc3f53678653c037e37e5e8b1c2cd98550
Karen Tano <thetanos24@gmail.com>
2026-02-09 22:07:38 -0700

    updated users route

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const mainRouter = require("./routes/index");
 
 const app = express();
-
 const { PORT = 3001 } = process.env;
 
 mongoose
@@ -11,6 +11,8 @@ mongoose
     console.log("Connected to DB");
   })
   .catch(console.error);
+
+app.use("/", mainRouter);
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
