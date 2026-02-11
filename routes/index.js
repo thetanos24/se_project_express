ace86831fa021d7b916b51ced4c190fe705db3da
Karen Tano <thetanos24@gmail.com>
2026-02-10 22:54:17 -0700

    updated to pass postman tests

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const itemRouter = require("./clothingitems");
 const { NOT_FOUND } = require("../utils/errors");
 
 router.use("/users", userRouter);
-router.use("/clothingitems", itemRouter);
+router.use("/items", itemRouter);
 router.use((req, res) => {
   res.status(NOT_FOUND).send({ message: "Requested resource not found" });
 });
