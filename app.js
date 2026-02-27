7917ad4f6c9a8a30da0b37ae92c8315c8fb41b87
Karen Tano <thetanos24@gmail.com>
2026-02-26 19:52:53 -0700

    added rate limiter and recommended helmet middleware

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const helmet = require("helmet");
+const limiter = require("./middlewares/rateLimiter");
 const mainRouter = require("./routes/index");
 
 const app = express();
 const { PORT = 3001 } = process.env;
+
+app.use(helmet());
+app.use(limiter);
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/wtwr_db")
