51e2e9c038766b90286632cff500c8d0af7bd088
Karen Tano <thetanos24@gmail.com>
2026-03-27 21:46:31 -0700

    updated missing export in clothing route

--- routes/clothingitems.js
+++ routes/clothingitems.js
@@ -20,3 +20,5 @@ router.post("/", validateClothingItem, createItem);
 router.delete("/:itemId", validateId, deleteItem);
 router.put("/:itemId/likes", validateId, likeItem);
 router.delete("/:itemId/likes", validateId, dislikeItem);
+
+module.exports = router;
